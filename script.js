7f0be72c0078749aedd61160c6ffe8eddeedf520
Ratul Hasan <ratulhasan9369@gmail.com>
2024-09-06 16:28:56 +0600

    Added min css

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 // script.js
-document.addEventListener('DOMContentLoaded', () => {
-    const menuButton = document.querySelector('.menu-button');
-    const navMenu = document.querySelector('.nav-menu');
-
-    menuButton.addEventListener('click', () => {
-        navMenu.classList.toggle('open');
-    });
+document.querySelector('.menu-button').addEventListener('click', function () {
+  this.classList.toggle('active');
+  document.querySelector('.nav-menu').classList.toggle('active');
 });
